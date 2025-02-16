52a3395ac4560c487ae72843f980d2b32b5fb862
captaindeng12138 <captaindeng12138@gmail.com>
2025-02-16 21:58:41 +0800

    Timer Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import { useState } from "react";
-
-import Header from "./components/Header";
-import Results from "./components/Results";
-import UserInput from "./components/UserInput";
+import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
-
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10
-  });
-
-  const inputIsValid = userInput.duration >= 1;
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput(prevUserInput => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: +newValue
-      };
-    })
-  }
-
-
   return (
     <>
-      <Header />
-      <UserInput onChange={handleChange} userInput={userInput} />
-      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
-      {inputIsValid && <Results input={userInput} />}
+      <Player />
+      <div id="challenges">
+        <TimerChallenge title={'Easy'} targetTime={1} />
+        <TimerChallenge title={'Not Easy'} targetTime={5} />
+        <TimerChallenge title={'Getting tough'} targetTime={10} />
+        <TimerChallenge title={'Pros only'} targetTime={15} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
