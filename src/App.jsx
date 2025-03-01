4c3ade0e78f1fef4dfe31124bbb9e35465bff99d
captaindeng12138 <captaindeng12138@gmail.com>
2025-03-01 15:03:39 +0800

    counter project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import Header from "./components/Header.jsx";
-import Quiz from "./components/Quiz.jsx";
+import { useState } from 'react';
+
+import Counter from './components/Counter/Counter.jsx';
+import Header from './components/Header.jsx';
+import { log } from './log.js';
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
 
 function App() {
+  log('<App /> rendered');
+
+  const [chosenCount, setChosenCount] = useState(0);
+
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
+    setChosenCount(prevCount => prevCount + 1);
+    console.log(chosenCount);
+  }
+
   return (
     <>
       <Header />
-      <Quiz />
+      <main>
+        <ConfigureCounter onSet={handleSetCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
+        <Counter initialCount={0} />
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
