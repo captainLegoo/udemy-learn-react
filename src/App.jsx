9cff847539224f4430aaeebfca1ebdf35c233d3c
captaindeng12138 <captaindeng12138@gmail.com>
2025-03-03 16:43:46 +0800

    react form

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
-
-import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
-import { log } from './log.js';
-import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
+import Login from './components/StateLogin.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
-  log('<App /> rendered');
-
-  const [chosenCount, setChosenCount] = useState(0);
-
-  function handleSetCount(newCount) {
-    setChosenCount(newCount);
-    setChosenCount(prevCount => prevCount + 1);
-    console.log(chosenCount);
-  }
-
   return (
     <>
       <Header />
       <main>
-        <ConfigureCounter onSet={handleSetCount} />
-        <Counter key={chosenCount} initialCount={chosenCount} />
-        <Counter initialCount={0} />
+        <Login />
+        {/* <Signup /> */}
       </main>
     </>
   );
