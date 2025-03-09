929f366a938dbc31577f87714ef60c3a05554235
captaindeng12138 <captaindeng12138@gmail.com>
2025-03-09 22:24:17 +0800

    react-toolkit

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Header.module.css";
+import { authActions } from "../store/auth";
+
+const Header = () => {
+  const dispath = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  // function logoutHandler() {
+  //   dispath(authActions.logout());
+  // }
+
+  const logoutHandler = () => {
+    dispath(authActions.logout());
+  }
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
--- src/components/UI/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({ children, textOnly, className, ...props }) {
-  const cssClasses = textOnly ? `text-button ${className}` : `button ${className}`;
-
-  return (
-    <button {...props} className={cssClasses}>
-      {children}
-    </button>
-  )
-};
