241729fffc265c71e5034fa4b27048a9551bc944
captaindeng12138 <captaindeng12138@gmail.com>
2025-03-21 01:48:08 +0800

    first project build by NextJs

--- frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import PageContent from "../components/PageContent";
-
-function HomePage() {
-  return <PageContent title="Welcome!">
-    <p>Browser all amazing events</p>
-  </PageContent>;
-}
-
-export default HomePage;
